6a3550857a548fe443187dd320a9783a0ad5e457
Rakesh8086 <rakeshteja7007@gmail.com>
2024-07-22 12:49:52 +0530

    added function to create duration matrix

--- matrix_creation.py
+++ matrix_creation.py
@@ -1,10 +1,20 @@
 
-def create_distance_matrix(df, distance_dictionary):
+def create_distance_matrix(dataframe, distance_dictionary):
     distance_matrix = []
-    for source in df.source.unique().tolist():
+    for source in dataframe.source.unique().tolist():
         distance_from_each_place_to_other = []
-        for destination in df.destination.unique().tolist():
+        for destination in dataframe.destination.unique().tolist():
             distance_from_each_place_to_other.append(distance_dictionary[source, destination])
         distance_matrix.append(distance_from_each_place_to_other)
 
     return distance_matrix
+
+def create_duration_matrix(dataframe, duration_dictionary):
+    duration_matrix = []
+    for source in dataframe.source.unique().tolist():
+        duration_1d = []
+        for destination in dataframe.destination.unique().tolist():
+            duration_1d.append(duration_dictionary[source, destination])
+        duration_matrix.append(duration_1d)
+
+    return duration_matrix
